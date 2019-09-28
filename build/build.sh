19f5d50489aa850c87784630cdce3c56a65261ee
adibrastegarnia <adib.rastegarnia@gmail.com>
2019-09-28 19:48:30 -0400

    Add build script

--- build/build.sh
+++ build/build.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+shopt -s extglob
+git config user.name "adibrastegarnia"
+git config user.email "arastega@purdue.edu"
+git remote add gh-token "https://${GH_TOKEN}@github.com/multi-repo-docs-versioning/root.git";
+git fetch gh-token && git fetch gh-token gh-pages:gh-pages;
+CURRENT_PATH=$PWD
+cd $CURRENT_PATH
+mkdocs gh-deploy -v --clean --remote-name gh-token;
